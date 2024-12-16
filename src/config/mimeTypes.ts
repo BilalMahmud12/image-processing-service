0ec0407718f84cb759dbf8c42c95e541c6257f76
Bilal <bm@fabricdata.com>
2024-12-16 14:06:14 +0300

    added config for mime types

---
+++ src/config/mimeTypes.ts
@@ -0,0 +1,10 @@
+export const imageMimeTypes = [
+    'image/jpeg',
+    'image/png',
+    'image/webp',
+    'image/gif',
+    'image/bmp',
+    'image/tiff',
+    'image/svg+xml',
+    'image/x-icon',
+]
